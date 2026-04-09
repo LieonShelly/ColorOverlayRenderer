c83f67f874692d2c0ffffd95fab31e8f711442ab
renjun <lieoncx@gmail.com>
2026-04-09 15:47:17 +0800

    Update

---
+++ ColorOverlayRenderer/ColorOverlayRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorOverlayRendererApp.swift
+//  ColorOverlayRenderer
+//
+//  Created by Renjun Li on 2026/4/9.
+//
+
+import SwiftUI
+
+@main
+struct ColorOverlayRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorOverlayRendererTests/ColorOverlayRendererTests.swift
@@ -0,0 +1,16 @@
+//
+//  ColorOverlayRendererTests.swift
+//  ColorOverlayRendererTests
+//
+//  Created by Renjun Li on 2026/4/9.
+//
+
+import Testing
+
+struct ColorOverlayRendererTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
